4075853b391d59abf00ca54ca6d68bccd96d388f
pedrxbro <pedrohdpaula@edu.univali.br>
2025-06-22 19:08:54 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/univali/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univali.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
